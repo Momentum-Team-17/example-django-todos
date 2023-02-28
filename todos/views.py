9f8fdacfb0a595b60c27848071a61ec94d0909d6
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2023-02-28 15:26:19 -0500

    working create view

--- todos/views.py
+++ todos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ItemForm
 from .models import Item
 
@@ -12,5 +12,13 @@ def list_todos(request):
 
 
 def create_todo(request):
+    if request.method == 'POST':
+        item_form = ItemForm(request.POST)
+        # form bound with data (filled out)
+        if item_form.is_valid():
+            # django checks if all the form data is valid
+            item_form.save()
+            # this saves a new instance of Item in the database using the form data. This works because we are inheriting from ModelForm
+            return redirect('home')
     form = ItemForm()
     return render(request, 'todos/new_todo.html', {'form': form})
